b40ee273aaa3cd7684745449b0b09ab75916176b
lm <super1742720898@gmail.com>
2020-06-07 21:55:56 +0800

    :arrow_up:Optimize details

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,6 +1,27 @@
 Page({
-    data: {},
-    onLoad: function (options) {
-
-    }
+    data: {
+        inputShowed: false,
+        inputVal: "",
+        banners: [
+            {url:'/images/goods/1.png'},
+            {url:'/images/goods/2.png'},
+            {url:'/images/goods/5.png'},
+            {url:'/images/goods/6.png'}
+        ],
+    },
+    onLoad() {
+        this.setData({
+            search: this.search.bind(this)
+        })
+    },
+    search: function (value) {
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                resolve([{text: '搜索结果', value: 1}, {text: '搜索结果2', value: 2}])
+            }, 200)
+        })
+    },
+    selectResult: function (e) {
+        console.log('select result', e.detail)
+    },
 });
